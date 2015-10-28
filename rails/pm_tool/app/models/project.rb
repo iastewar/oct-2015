7d965f9a47b066532265504aae4e97930fa69b03
Ian Stewart <iastewar555@gmail.com>
2015-10-28 14:53:58 -0700

    commented associations

--- rails/pm_tool/app/models/project.rb
+++ rails/pm_tool/app/models/project.rb
@@ -1,5 +1,6 @@
 class Project < ActiveRecord::Base
 #  serialize :task_array, Array
+#  has_many :tasks, dependent: :destroy
 
   validates :title, presence: true,
                     uniqueness: true
